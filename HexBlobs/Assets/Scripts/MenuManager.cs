b6dae4e6c08362a6d804dcc616713275a8619a47
Aaron <amccurry209@gmail.com>
2026-04-14 16:09:56 -0400

    Wired up main menu buttons and scene transitions

---
+++ HexBlobs/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadGameScene1()
+    {
+        SceneManager.LoadScene("GameScene1");
+    }
+
+    public void LoadGameScene2()
+    {
+        SceneManager.LoadScene("GameScene2");
+    }
+
+    public void LoadMatchHistory()
+    {
+        SceneManager.LoadScene("MatchHistory");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
